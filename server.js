057430a82c296525e64dbbaca955cd3dcf1dfca6
var-greyShader <yasharyan307@outlook.com>
2020-06-02 23:59:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static(__dirname + '/node_modules'));
 app.use(express.static(__dirname + '/public'));
 
 app.get("/", function (req, res) {
