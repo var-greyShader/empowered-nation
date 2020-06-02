66f8ff18e6f6ef8fee451ab3c6e4b1f6e2bfccdb
var-greyShader <yasharyan307@outlook.com>
2020-06-02 22:32:15 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/public/home.html")
+})
+
+app.get("/project", function (req, res) {
+    res.sendFile(__dirname + "/public/projects.html")
+})
+
+app.get("/team", function (req, res) {
+    res.sendFile(__dirname + "/public/team.html")
+})
+
+app.get("/dbox", function (req, res) {
+    res.sendFile(__dirname + "/public/dbox.html")
+})
+
+app.get("*", function (req, res) {
+    res.sendFile(__dirname + "/public/error.html")
+})
+
+app.listen(3000);
